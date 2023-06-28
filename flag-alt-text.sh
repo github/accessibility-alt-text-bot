e38946d916a6f1663bf842ff4efdc958e0bfab3b
Kendall Gassner <kendallgassner@github.com>
2023-06-28 00:45:31 +0000

    Flag images without alt text

--- flag-alt-text.sh
+++ flag-alt-text.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 flagAltText() {
-  markdownMacOsScreenshotRegex="^.*!\[(Clean|Screen) ?[S|s]hot [0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-9][0-9].*\].*$"
-  semanticMacOsScreenshotRegex="^.*<img.*(Clean|Screen) ?[S|s]hot [0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-9][0-9].*$"
-  markdownImageRegex="^.*!\[(i|I)mage\].*$"
-  semanticImageRegex="^.*<img.*alt=\"(i|I)mage\".*$"
-  if  [[ $1 =~ $semanticMacOsScreenshotRegex ]] || [[ $1 =~ $markdownMacOsScreenshotRegex ]] || [[ $1 =~ $semanticImageRegex ]] || [[ $1 =~ $markdownImageRegex ]]; then
-    echo true
-  else
-    echo false
-  fi
+    markdownMacOsScreenshotRegex="^.*!\[(Clean|Screen) ?[S|s]hot [0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-9][0-9].*\].*$"
+    semanticMacOsScreenshotRegex="^.*<img.*(Clean|Screen) ?[S|s]hot [0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-9][0-9].*$"
+    markdownImageRegex="^.*!\[(i|I)mage\].*$"
+    semanticImageRegex="^.*<img.*alt=\"(i|I)mage\".*$"
+    emptySemanticRegex="^.*<img.*alt=(\"|')(\"|').*$"
+    withoutAltRegex="^.*<img((?!alt=).)>.*$"
+    emptyMarkdownRegex="^.*!\[\].*$"
+
+    if [[ $1 =~ $withoutAltRegex ]] || [[ $1 =~ $semanticMacOsScreenshotRegex ]] || [[ $1 =~ $markdownMacOsScreenshotRegex ]] || [[ $1 =~ $semanticImageRegex ]] || [[ $1 =~ $markdownImageRegex ]]  || [[ $1 =~ $emptySemanticRegex ]] || [[ $1 =~ $emptyMarkdownRegex ]]; then
+        echo true
+    else
+        echo false
+    fi
 }
--- test-flag-alt-text.sh
+++ test-flag-alt-text.sh
@@ -6,47 +6,54 @@ source assert.sh
 # true
 
 declare -a should_be_true=(
-  # markdown
-  '![Cleanshot 2020-01-01 at 12.00.00.png]' 
-  '![Clean shot 2020-12-01 @12x]'
-  "![Clean shot 2020-12-01 @12x]"
-  "![Screen Shot 2020-01-01 at 12.00.00.png]"
-  "![Screenshot 2020-01-01 at 12.00.00.png]"
-  "![image]"
-  "![Image]"
-  "Check this: ![Image]"
-  "My awesome ![image]"
-  'Check this out: <img alt="image" src="cat.png">'
-  # html
-  '<img alt="image" src="cat.png">'
-  '<img alt="Screen shot 2020-01-01 at 12.00.00.png" src="cat.png">'
-  '<img alt="Screen Shot 2020-01-01 at 12.00.00.png" src="cat.png">'
-  '<img alt="Screenshot 2020-01-01 at 12.00.00.png" src="cat.png">'
-  '<img alt="CleanShot 2020-01-01 @12x" src="cat.png">'
+    # markdown
+    '![Cleanshot 2020-01-01 at 12.00.00.png]'
+    '![Clean shot 2020-12-01 @12x]'
+    "![Clean shot 2020-12-01 @12x]"
+    "![Screen Shot 2020-01-01 at 12.00.00.png]"
+    "![Screenshot 2020-01-01 at 12.00.00.png]"
+    "![image]"
+    "![Image]"
+    "![]"
+    "Check this: ![Image]"
+    "My awesome ![image]"
+    'Check this out: <img alt="image" src="cat.png">'
+    # html
+    '<img alt="image" src="cat.png">'
+    '<img alt="" src="cat.png">'
+    "<img alt='' src="cat.png">"
+    '<img src="cat.png">'
+    '<img alt src="cat.png">'
+    '<img src="cat.png" width="10px">'
+    '<img alt="Screen shot 2020-01-01 at 12.00.00.png" src="cat.png">'
+    '<img alt="Screen Shot 2020-01-01 at 12.00.00.png" src="cat.png">'
+    '<img alt="Screenshot 2020-01-01 at 12.00.00.png" src="cat.png">'
+    '<img alt="CleanShot 2020-01-01 @12x" src="cat.png">'
 )
 
 declare -a should_be_false=(
-  # markdown
-  "![Screenshot of the new GitHub home page]" 
-  "![Screen shot of Submit button with updated color contrast.]"
-  "![Image of a cat]"
-  # html
-  '<img alt="Mona Lisa, the Octocat" src="cat.png">'
-  '<img alt="Screenshot of the new danger button with a dark red shade" src="test.png">'
-  '<img alt="Clean shot of the scenery" src="test.png">'
+    # markdown
+    "![Screenshot of the new GitHub home page]"
+    "![Screen shot of Submit button with updated color contrast.]"
+    "![Image of a cat]"
+    # html
+    '<img src="cat.png" alt="Mona Lisa, the Octocat" >'
+    '<img alt="Mona Lisa, the Octocat" src="cat.png">'
+    '<img alt="Screenshot of the new danger button with a dark red shade" src="test.png">'
+    '<img alt="Clean shot of the scenery" src="test.png">'
 )
 
 echo "******Expecting true:*******"
 for i in "${should_be_true[@]}"
 do
-  echo "Testing: $i"
-  assert_true "$(flagAltText "$i")" "$i must be true"
+    echo "Testing: $i"
+    assert_true "$(flagAltText "$i")" "$i must be true"
 done
 
 echo "******Expecting false:*******"
 for i in "${should_be_false[@]}"
 do
-  echo "Testing: $i"
-  assert_false "$(flagAltText "$i")" "$i must be false"
+    echo "Testing: $i"
+    assert_false "$(flagAltText "$i")" "$i must be false"
 done
 
