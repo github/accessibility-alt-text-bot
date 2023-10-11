622bdd51f8fda7b0225f0e0989d5bd4dd3cba8dd
Kendall Gassner <kendallgassner@github.com>
2023-10-11 14:01:22 -0700

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { validate } from "./validate.js";
 const [content] = process.argv.slice(2);
 
 const run = async () => {
-  console.log((await validate(content)).length);
+  console.log((await validate(content)).length > 0);
 };
 
 run();
