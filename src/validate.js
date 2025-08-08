6dd5aa1cfd0544cd44ced8c52a1fe14bd0b779e2
Joyce Zhu <joycezhu@github.com>
2025-08-08 01:23:17 -0400

    Attempt to update `markdownlint` breaking changes

--- src/validate.js
+++ src/validate.js
@@ -1,24 +1,30 @@
+import markdownIt from "markdown-it";
 import markdownlint from "markdownlint";
-import yaml from 'js-yaml';
-import markdownlintGitHub from "@github/markdownlint-github";
+import yaml from "js-yaml";
+const markdownItFactory = () => markdownIt({ html: true });
 
 export const validate = (markdown, config) => {
-  const configObject = yaml.load(config)
-  return (markdownlint
-    .sync({
-      strings: {
-        content: markdown,
-      },
-      config: config ? { default: false, ...configObject } : {
-        default: false,
-        "no-default-alt-text": true,
-        "no-alt-text": true,
-        "no-empty-alt-text": true,
-      },
-      handleRuleFailures: true,
-      customRules: markdownlintGitHub,
-    })
-    .content?.map((error) => {
-      return `- ${error.ruleDescription} at line ${error.lineNumber}`;
-    }) ?? []).join("\n");
-}
+  const configObject = yaml.load(config);
+  return (
+    markdownlint
+      .sync({
+        strings: {
+          content: markdown,
+        },
+        config: config
+          ? { default: false, ...configObject }
+          : {
+              default: false,
+              "no-default-alt-text": true,
+              "no-alt-text": true,
+              "no-empty-alt-text": true,
+            },
+        handleRuleFailures: true,
+        markdownItFactory,
+        customRules: ["@github/markdownlint-github"],
+      })
+      .content?.map((error) => {
+        return `- ${error.ruleDescription} at line ${error.lineNumber}`;
+      }) ?? []
+  ).join("\n");
+};
