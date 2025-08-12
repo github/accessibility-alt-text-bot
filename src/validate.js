a91803019937f6eb34d058bfcff8d088c6aa9d2b
Joyce Zhu <joycezhu@github.com>
2025-08-12 18:33:36 +0000

    is this the new import

--- src/validate.js
+++ src/validate.js
@@ -1,30 +1,28 @@
 import markdownIt from "markdown-it";
-import markdownlint from "markdownlint";
+import { lint } from "markdownlint/sync";
 import yaml from "js-yaml";
 const markdownItFactory = () => markdownIt({ html: true });
 
 export const validate = (markdown, config) => {
   const configObject = yaml.load(config);
   return (
-    markdownlint
-      .sync({
-        strings: {
-          content: markdown,
-        },
-        config: config
-          ? { default: false, ...configObject }
-          : {
-              default: false,
-              "no-default-alt-text": true,
-              "no-alt-text": true,
-              "no-empty-alt-text": true,
-            },
-        handleRuleFailures: true,
-        markdownItFactory,
-        customRules: ["@github/markdownlint-github"],
-      })
-      .content?.map((error) => {
-        return `- ${error.ruleDescription} at line ${error.lineNumber}`;
-      }) ?? []
+    lint({
+      strings: {
+        content: markdown,
+      },
+      config: config
+        ? { default: false, ...configObject }
+        : {
+            default: false,
+            "no-default-alt-text": true,
+            "no-alt-text": true,
+            "no-empty-alt-text": true,
+          },
+      handleRuleFailures: true,
+      markdownItFactory,
+      customRules: ["@github/markdownlint-github"],
+    }).content?.map((error) => {
+      return `- ${error.ruleDescription} at line ${error.lineNumber}`;
+    }) ?? []
   ).join("\n");
 };
